ba1532fb03418c45c532b08c968339457f85165e
royalbalaji319-cloud <royalbalaji319@gmail.com>
2026-07-09 08:24:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("balaji123", "secret")
+    SECRET_KEY = os.getenv("balaji123")
 
     MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
     MYSQL_USER = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("826412")
     MYSQL_DB = os.getenv("MYSQL_DB")
-    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
 
     ADZUNA_APP_ID = os.getenv("e233a6d7")
     ADZUNA_APP_KEY = os.getenv("14b5d769fb00f54b274d57b2486fdb06")
