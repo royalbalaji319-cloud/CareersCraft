4f0271caff83b532ba754e461305c36571483bf8
royalbalaji319-cloud <royalbalaji319@gmail.com>
2026-07-09 08:14:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,19 @@ from routes.auth import auth
 
 app = Flask(__name__)
 
-# Load configuration
 app.config.from_object(Config)
 
-# Initialize MySQL
 mysql.init_app(app)
 
-# Register Blueprints
 app.register_blueprint(auth)
 
-# Home Page
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Health Check (for testing)
-@app.route("/health")
-def health():
-    return "CareersCraft is Running Successfully!"
+@app.route("/test")
+def test():
+    return "Render Working Successfully!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
